9ee4ae4a90860952178bde5faa3c3d6b970c16b6
MythSnipper <mythsnipper2022@gmail.com>
2025-11-18 21:57:08 +0200

    vel

---
+++ 94.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void vel(TreeNode* node, vector<int>& niko){
+        if(!node){
+            return;
+        }
+        vel(node->left, niko);
+        niko.push_back(node->val);
+        vel(node->right, niko);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> miku;
+        vel(root, miku);
+        return miku;
+    }
+};
