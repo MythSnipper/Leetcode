ba10802f51fea493bc25caa22b91496a71ee3858
MythSnipper <mythsnipper2022@gmail.com>
2025-11-19 15:08:51 +0200

    v el

--- 3.cpp
+++ 3.cpp
@@ -1,31 +1,22 @@
 class Solution {
     public:
         int lengthOfLongestSubstring(string s) {
-            unordered_set<char> vel;
+            if(s.empty())return 0;
+            unordered_map<char, int> vel;
             int longest = 0;
             
             int slowptr = 0;
             for(int i=0;i<s.length();i++){
                 char cuerert = s[i];
-                cout << "Doing: " << cuerert << "\n";
                 if(vel.find(cuerert) != vel.end()){ //found
-                    cout << "REPEATED! new length " << (i-slowptr) << "\n";
-                    //new longest substring
-                    longest = (i-slowptr > longest) ? i-slowptr : longest;
-                    //move slow ptr until good
-                    while(s[slowptr] != cuerert){
-                        cout << "SLOWPTR is " << slowptr << ", char " << s[slowptr] << "\n";
-                        vel.erase(s[slowptr]);
-                        slowptr++;
+                    if(vel[cuerert] >= slowptr){
+                        //new longest substring
+                        longest = (i-slowptr > longest) ? i-slowptr : longest;
+                        //move slow ptr until good
+                        slowptr = vel[cuerert] + 1;
                     }
-                    slowptr++;
-    
-                    cout << "erased, now fast is " << i << " and slow is " << slowptr << "\n";
                 }
-                else{
-                    cout << "NOT FOUND\n";
-                    vel.insert(cuerert);
-                }
+                vel[cuerert] = i;
             }
             longest = (s.length()-slowptr > longest) ? s.length()-slowptr : longest;
             return longest;
